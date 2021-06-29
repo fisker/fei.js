63f79ed07d4349d0ea7395dc0faa0519fe1ab179
fisker Cheung <lionkay@gmail.com>
2021-06-29 09:46:50 +0800

    Meta tweak

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import cjs from '@rollup/plugin-commonjs'
 import rollupPrettier from 'rollup-plugin-prettier'
