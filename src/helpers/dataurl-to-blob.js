68c1c23021d5b1250d1bec6e57b0b84d17fc1d48
fisker Cheung <lionkay@gmail.com>
2021-12-15 16:20:18 +0800

    Update dataurl-to-blob.js

--- src/helpers/dataurl-to-blob.js
+++ src/helpers/dataurl-to-blob.js
@@ -2,7 +2,7 @@ import {atob, Uint8Array, Blob} from './global-this.js'
 
 function dataURLToBlob(data, type) {
   const binary = atob(data.split(',')[1])
-  const array = Uint8Array.from(binary, (byte) => byte.charCodeAt(0))
+  const array = Uint8Array.from(binary, (byte) => byte.codePointAt(0))
 
   return new Blob([array], {
     type,
